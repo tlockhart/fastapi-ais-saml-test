4a7cb0d0c2dc0a634d3296ab59afff0461d16465
Tony Lockhart <tony.lockhart@ymail.com>
2026-03-18 12:39:46 -0400

    Create settings.py

---
+++ utils/saml/settings.py
@@ -0,0 +1,18 @@
+from copy import deepcopy
+
+from saml import settings as saml_settings_source
+from saml import advanced_settings as saml_advanced_settings
+
+from utils.dict_utils import _deep_merge
+
+
+def get_saml_configs() -> dict:
+    """
+    Load base SAML settings and apply any advanced overrides.
+    """
+    source_settings = getattr(saml_settings_source, "SOURCE_CONFIG", {})
+    result = deepcopy(source_settings)
+    advanced_settings = getattr(saml_advanced_settings, "ADVANCED_CONFIG", None)
+    if isinstance(advanced_settings, dict):
+        _deep_merge(result, advanced_settings)
+    return result
